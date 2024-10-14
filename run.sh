866c8459f1c869bb50965700a2ea927c606990a2
AR-92 <muhammadahmedrana1992@yahoo.com>
2024-10-14 13:10:14 +0500

    open browser

--- run.sh
+++ run.sh
@@ -7,5 +7,8 @@ kill $(lsof -t -i:8000)
 # Start the Python server in the background
 python3 -m http.server 8000 &
 
+# Open the link in the browser
+xdg-open http://localhost:8000/src/
+
 # Run Tailwind CSS in watch mode in the foreground
-tailwindcss -i ./main.css -o ./src/assets/css/tailwindcss.css --watch
+tailwindcss -i ./main.css -o ./src/assets/css/tailwindcss.css --watch 
